d8d9cda804e8a90bc0934a85574b7d274616806e
Venkat Garapati <venkat_garapati@outlook.com>
2017-06-25 13:43:49 -0400

    Zankyou Service Project added.

---
+++ ZankyouService/Server.Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZankyouService
+{
+    using NUnit.Framework;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    [TestFixture]
+    public class ServerTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Server server = new Server();
+        }
+
+        [Test()]
+        public void TestConnect()
+        {
+            Socket client = new Socket(SocketType.Stream, ProtocolType.IP);
+            client.Connect("127.0.0.1", 12345);
+            client.Close();
+        }
+
+        [Test()]
+        public void TestMultiConnect()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Socket client = new Socket(SocketType.Stream, ProtocolType.IP);
+                client.Connect("127.0.0.1", 12345);
+            }
+        }
+
+        [Test()]
+        public void TestMessage()
+        {
+            Socket client = new Socket(SocketType.Stream, ProtocolType.IP);
+            client.Connect("127.0.0.1", 12345);
+            client.Send(Encoding.ASCII.GetBytes("hello world"));
+            client.Close();
+        }
+
+        [Test()]
+        public void TestMuliClientMessages()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Socket client = new Socket(SocketType.Stream, ProtocolType.IP);
+                client.Connect("127.0.0.1", 12345);
+                client.Send(Encoding.ASCII.GetBytes("hello world"));
+            }
+        }
+    }
+}
---
+++ ZankyouService/Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZankyouService
+{
+    public class Server
+    {
+        private TcpListener serverListener;
+        
+        /// <summary>
+        /// Creates a sockets that listens on the specified port,
+        /// configured in the user configuration.
+        /// </summary>
+        public Server(string ipAddress = "127.0.0.1", int port = 12345)
+        {
+            var localHost = IPAddress.Parse(ipAddress);
+            serverListener = new TcpListener(localHost, port);
+            serverListener.Start();
+            serverListener.BeginAcceptSocket(this.AcceptClient, serverListener);
+        }
+
+        /// <summary>
+        /// Accepts a client asynchronously, allowing the 
+        /// TcpListener to continue listening for new clients.
+        /// </summary>
+        /// <param name="ar">The asychronous response representing the tcp listener</param>
+        private void AcceptClient(IAsyncResult ar)
+        {
+            var listener = (TcpListener) ar.AsyncState;
+            if (listener != null)
+            {
+                try
+                {
+                    Socket clientSocket = listener.EndAcceptSocket(ar);
+                    StartReceive(clientSocket);
+                    
+                } catch
+                {
+                    Console.WriteLine("Error with client connection");
+                }
+                Console.WriteLine("Client successfully connected");
+                listener.BeginAcceptSocket(this.AcceptClient, listener);
+            }
+        }
+
+        /// <summary>
+        /// Waits for messages from client asynchronously.
+        /// </summary>
+        /// <param name="clientSocket">The client socket that is receiving messages.</param>
+        private void StartReceive(Socket clientSocket)
+        {
+            byte[] buffer = new byte[200];
+            try
+            {
+                clientSocket.BeginReceive(buffer, 0, 200, SocketFlags.None, (state) =>
+                {
+                    try
+                    {
+                        if (clientSocket.Connected)
+                        {
+                            int bytesReceived = clientSocket.EndReceive(state);
+                            if (bytesReceived != 0)
+                            {
+                                string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                                Console.WriteLine(message);
+                                StartReceive(clientSocket);
+                            }
+                        }
+                    } catch
+                    {
+                        Console.WriteLine("Error with connected client");
+                    }
+                }, clientSocket);
+            } catch
+            {
+                Console.WriteLine("Error with connected client");
+            }
+        }
+    }
+}
