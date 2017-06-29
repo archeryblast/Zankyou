1c978fb1315943c4379653189d6aeb96f90ddaf7
Venkat Garapati <venkat_garapati@outlook.com>
2017-06-28 21:33:09 -0400

    Configuration and configuration test added

---
+++ ZankyouService/Configuration.Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ZankyouService
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void getAvailbeHostNamesTest()
+        {
+            List<IPAddress> list = Configuration.GetAvailbeIPAddresses();
+            foreach (var l in list)
+            {
+                Console.WriteLine(l.ToString());
+            }
+        }
+
+    }
+}
---
+++ ZankyouService/Configuration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ZankyouService
+{
+    public static class Configuration
+    {
+        private static int port;
+        private static IPAddress hostname;
+
+        public static int Port { get { return port; } set { port = value; } }
+        public static string Hostname
+        {
+            get
+            {
+                return hostname.ToString();
+            }
+        }
+
+        public static List<IPAddress> GetAvailbeIPAddresses()
+        {
+            var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+            var result = host.AddressList.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToList();
+            return result;
+        }
+
+        public static void SetHostname(IPAddress selected)
+        {
+            hostname = selected;
+        }
+
+    }
+}
